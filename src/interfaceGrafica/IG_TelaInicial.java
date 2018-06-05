4f353a9cba4e1fe9dbdca071eb3f6e5ed28f04df
104969 <104969@SALA409.proway.treina>
2018-06-04 21:07:48 -0300

    Criado

---
+++ src/interfaceGrafica/IG_TelaInicial.java
@@ -0,0 +1,42 @@
+package interfaceGrafica;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+
+public class IG_TelaInicial extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					IG_TelaInicial frame = new IG_TelaInicial();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public IG_TelaInicial() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+	}
+
+}
---
+++ src/main/quiz.java
@@ -0,0 +1,10 @@
+package main;
+
+public class quiz {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
